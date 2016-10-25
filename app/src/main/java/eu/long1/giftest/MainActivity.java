b5b2015ce52ed98b2debe43e8d43415a4d942a08
Razvan Cristian Lung <lung.razvan@gmail.com>
2016-10-25 20:47:32 +0300

    Initial commit

---
+++ app/src/main/java/eu/long1/giftest/MainActivity.java
@@ -0,0 +1,37 @@
+package eu.long1.giftest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText gifTextView = (EditText) findViewById(R.id.textView);
+
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        sb.append("Text followed by animated gif: ");
+        String dummyText = "dummy";
+        sb.append(dummyText);
+        try {
+            sb.setSpan(new AnimatedImageSpan(new AnimatedGifDrawable(getAssets().open("agif.gif"), new AnimatedGifDrawable.UpdateListener() {
+                @Override
+                public void update() {
+                    gifTextView.postInvalidate();
+                }
+            })), sb.length() - dummyText.length(), sb.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gifTextView.setText(sb);
+    }
+}
